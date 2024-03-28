c5defe33b5fcefe8a594647f5a6aeca03fac3f38
Temi Makinde <Temi.Makinde@Gmail.com>
2024-03-28 16:05:24 +0000

    Added more unit tests for the clients

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+function add(a, b) {
+  return a + b;
+}
+
 test("renders learn react link", () => {});
+
+describe("add function", () => {
+  it("adds two positive numbers correctly", () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  it("adds a positive and a negative number correctly", () => {
+    expect(add(5, -3)).toBe(2);
+  });
+
+  it("adds two negative numbers correctly", () => {
+    expect(add(-2, -3)).toBe(-5);
+  });
+
+  it("adds zero correctly", () => {
+    expect(add(5, 0)).toBe(5);
+  });
+});
